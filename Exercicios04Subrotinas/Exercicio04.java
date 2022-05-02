9312815afe503a9769a345ac2d3a68da05298ccb
Rafa Raizer <87991807+RAFARZ76@users.noreply.github.com>
2022-05-02 16:59:20 -0300

    Add files via upload

---
+++ Exercicios04Subrotinas/Exercicio04.java
@@ -0,0 +1,49 @@
+package br.com.subrotinas;
+import java.util.Scanner;
+
+public class Exercicio04 {
+
+	public static void main(String[] args) {
+
+		int numeros[] = new int[10];
+		
+		preencherVetor(numeros);
+		
+		System.out.println();
+		
+		printImpar(numeros);
+		
+		System.out.println();
+		
+		printPar(numeros);
+
+	}
+
+	
+	public static void preencherVetor(int v[]) {
+		Scanner leitor = new Scanner(System.in);
+		
+		for (int i = 0; i < v.length; i++) {
+			System.out.printf("v[%d] = ", i);
+			v[i] = leitor.nextInt();
+		}
+		
+		leitor.close();
+	}
+	
+	public static void printImpar(int v[]) {
+		for (int i = 0; i < v.length; i++) {
+			if (v[i] % 2 != 0) {
+				System.out.println(v[i]);
+			}
+		}
+	}
+	
+	public static void printPar(int v[]) {
+		for (int i = 0; i < v.length; i++) {
+			if (v[i] % 2 == 0) {
+				System.out.println(v[i]);
+			}
+		}
+	}
+}
---
+++ JavaPOOExercicio02/src/br/com/JavacomPOO/Teste.java
@@ -0,0 +1,34 @@
+package br.com.JavacomPOO;
+// Classe
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+	 //Classes
+		Data dtNasc = new Data();
+	  
+	  /*Atribuindo valores aos atributos do objeto e preenchendo os dados
+	  data1.dia = 19;
+	  data1.mes = 01;
+	  data1.ano = 1976;
+		*/
+		
+		//Chamando o método para preencherrdata
+		 dtNasc.preencherData(19, 01, 1976);
+	  
+	  //Chamando o método para imprimirdata
+	  //dtNasc.imprimirData();
+	  
+	  Pessoa Rafa = new Pessoa();
+	 /* Rafa.nome = "Rafa Raizer";
+	  Rafa.documento = "016.364.389/03";
+	  */
+	  
+	  Rafa.preencherDados("Rafa Raizer", "016.364.389/03");
+	  Rafa.dataNascimento = dtNasc;	
+	  
+	  Rafa.imprimir();
+	 
+	}
+}
