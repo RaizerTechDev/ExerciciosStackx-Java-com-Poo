4e5559b29a7624c16b14c0340cc2f1d7466bd619
Rafa Raizer <87991807+RAFARZ76@users.noreply.github.com>
2022-05-02 16:57:56 -0300

    Add files via upload

---
+++ Exemplos FundamentosJava/Exemplo03.java
@@ -0,0 +1,18 @@
+package br.com.fundamentos;
+
+public class Exemplo03 {
+
+	public static void main(String[] args) {
+		boolean foiPago = true;
+		System.out.println("foiPago?: " + foiPago);
+
+		if (foiPago) {
+			System.out.println("Não te devo mais nada!");
+		} else { 
+			System.out.println("Opss!! Desculpa o meu descuido!");
+		}
+			
+		}
+	}
+
+
---
+++ Exemplos FundamentosJava/Exemplo04.java
@@ -0,0 +1,20 @@
+package br.com.fundamentos;
+
+public class Exemplo04 {
+
+	public static void main(String[] args) {
+	   char letra;
+	   
+	   letra = 'A';
+		
+      System.out.println(letra);	
+	   
+	   String texto;
+	   
+	   texto = "Aqui está um texto";
+			   
+		System.out.println("CONTEUDO: " + texto);	   
+			   
+	}
+
+}
