f6e3f8a1d99bbcc3f731e02e7dbc2e072fb4ed7b
Rafa Raizer <87991807+RAFARZ76@users.noreply.github.com>
2022-05-02 17:01:21 -0300

    Add files via upload

---
+++ JavaPOOExercicios01/AulaRepetição/src/Exemplo04For.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Exemplo04For {
+	
+	public static void main(String[] args) {
+	
+
+		
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Digite um valor de Inicio:");
+		int inicio = leitor.nextInt();
+		
+		System.out.println("Digite um valor de Fim:");
+		int fim = leitor.nextInt();
+		
+		for (int i = inicio; i <= fim; i++) {
+			System.out.println(i);
+			
+					
+		}
+
+	}
+
+}
+
+
