1e6a3baf2c6259d6edd349c462dede5423e4f536
Rafa Raizer <87991807+RAFARZ76@users.noreply.github.com>
2022-05-02 15:05:10 -0300

    Add files via upload

---
+++ Exercicio01Fundamentos/br/com/fundamentos/Exemplo05.java
@@ -0,0 +1,29 @@
+package br.com.fundamentos;
+
+public class Exemplo05 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 3;
+		int res;
+		
+		//Soma
+		 res = a + b;
+		 System.out.println("SOMA: " + res);
+		 
+		//Subtração
+		 res = a - b;
+		 System.out.println("SUBT: " + res);
+		 
+		//Multiplicação
+		 res = a * b;
+		 System.out.println("MULT: " + res);
+		 
+		//Soma
+		 res = a / b;
+		 System.out.println("DIVS: " + res);
+	
+
+	}
+
+}
