ce3b5686bbd55f2ce023f5d8d30f5b9a258a5722
Rafa Raizer <87991807+RAFARZ76@users.noreply.github.com>
2022-05-02 16:19:14 -0300

    Add files via upload

---
+++ Exemplos FundamentosJava/Exemolo02.java
@@ -0,0 +1,19 @@
+package br.com.fundamentos;
+
+public class Exemolo02 {
+
+	public static void main(String[] args) {
+		double salario; 
+		
+		 salario = 2800.00;
+		System.out.println(salario);
+		
+		float taxa;
+		
+		taxa = 2.5f;
+		
+		System.out.println(taxa);
+
+	}
+
+}
