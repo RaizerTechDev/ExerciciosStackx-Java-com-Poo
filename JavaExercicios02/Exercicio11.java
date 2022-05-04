de19a185dcc01d4dcbf33e1655a375b72addaec9
Rafa Raizer <87991807+RAFARZ76@users.noreply.github.com>
2022-05-04 14:34:35 -0300

    Add files via upload

---
+++ JavaExercicios02/Exercicio11.java
@@ -0,0 +1,34 @@
+package br.com.JavaExercicios02;
+import java.util.Scanner;
+
+//Criar um programa que receba pelo teclado um número positivo inteiro N. 
+//Apresentar na tela a soma dos N primeiros números,
+//por exemplo: se N = 5, então SOMA = 1 + 2 + 3 + 4 + 5
+
+public class Exercicio11 {
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		// Criar um numero positivo inteiro N
+		int n, soma = 0;
+		
+		//Imprimir a leitura do Valor N
+		System.out.println("Digite o valor N:");
+		n = leitor.nextInt();
+		
+		// apresentar a soma na tela  
+		for (int i = 1; i <= n; i++) {
+			
+     	soma = soma + i;
+		}
+		//Imprimir a soma na tela  
+		System.out.printf("SOMA: %s", soma);
+
+	}
+
+}
+
+
+
+//1 2 3 4 5 6 7 8 9 10 11 12 13 1 41 51 61 7 18 19 20
---
+++ JavaExercicios02/Exercicio8.java
@@ -0,0 +1,36 @@
+package br.com.JavaExercicios02;
+
+import java.util.Scanner;
+
+//Criar um programa em Java que receba pelo teclado 10 numeros inteiros
+//e informe quantos numeros digitados são impares.
+
+public class Exercicio8 {
+
+	public static void main(String[] args) {
+		Scanner leitor = new Scanner(System.in);
+		
+		int contadorImpar = 0; //Variavel que vai contar os impares
+		
+		//Impressao 
+		System.out.println();
+		
+		//Criar programa que vai digitar os numeros inteiros no teclado
+		for (int i = 1; i <= 10; i++) {
+			
+			//Impressão da leitura da digitação desse numeros inteiros
+			System.out.printf("Digite o %d� n�mero: ", i);
+			int n = leitor.nextInt();
+			
+			//contagem dos numeros impares
+			int mod = n % 2;
+					
+			if (mod == 1) {		
+			contadorImpar++;
+			}
+		}
+		
+		//impressao da contagem de numeros impares
+		System.out.printf("Quantidade de impares: " + contadorImpar);
+	}
+}
